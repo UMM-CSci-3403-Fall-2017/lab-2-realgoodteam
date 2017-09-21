b41138c1d8e517df5b33fdcc077a11e9f071190c
holma198 <holma198@morris.umn.edu>
2017-09-21 12:00:47 -0500

    Add code to disemvowel.c, needs work, but no errors are returned.  Will need to return given string without the vowels

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,7 +1,30 @@
 #include <stdlib.h>
-
+#include <string.h>
 #include "disemvowel.h"
 
 char* disemvowel(char* str) {
-  return (char*) "";
+  int i;
+  int nonVowelCheck=0;
+  char* noVowel;
+  int len = strlen(str);
+  //char* vowels = calloc(11, sizeof(char));
+  char vowels[11] = {'a','A','e','E','i','I','o','O','u','U','\0'};
+  char* strPointer;  
+  for (i = 0; i < len; i++){
+	strPointer = str+i;
+  	if(strstr(vowels,strPointer)==NULL){
+		nonVowelCheck++;	
+	}
+  }
+  noVowel = calloc(nonVowelCheck+1,sizeof(char));
+  nonVowelCheck = 0;
+  for (i=0;i<strlen(str);i++){
+	strPointer = str+i;
+  	if(strstr(vowels,strPointer)==NULL){
+		nonVowelCheck++;
+		noVowel[nonVowelCheck] = str[i];
+	}
+
+  }
+  return noVowel;
 }
