d7c0ac70552e2316c28c7af8bef8ee768dd2fd88
holma198 <holma198@morris.umn.edu>
2017-09-22 15:28:50 -0500

    Improve code, now the return string does not return vowels, but returns consonants twice where the vowels would be.  Needs Work.

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -4,27 +4,36 @@
 
 char* disemvowel(char* str) {
   int i;
-  int nonVowelCheck=0;
+  int j;
+  int found;
+  int VowelCheck=0;
+  int nonVowelCheck = 0;
   char* noVowel;
   int len = strlen(str);
   //char* vowels = calloc(11, sizeof(char));
   char vowels[11] = {'a','A','e','E','i','I','o','O','u','U','\0'};
-  char* strPointer;  
   for (i = 0; i < len; i++){
-	strPointer = str+i;
-  	if(strstr(vowels,strPointer)==NULL){
-		nonVowelCheck++;	
+	for(j = 0; j < 11; j++){
+  		if(vowels[j] == str[i]) {
+			VowelCheck++;
+		}
 	}
   }
-  noVowel = calloc(nonVowelCheck+1,sizeof(char));
-  nonVowelCheck = 0;
-  for (i=0;i<strlen(str);i++){
-	strPointer = str+i;
-  	if(strstr(vowels,strPointer)==NULL){
-		nonVowelCheck++;
+  noVowel = calloc(len - VowelCheck+1,sizeof(char));
+  VowelCheck = 0;
+  for (i=0;i<strlen(str) + VowelCheck;i++){
+	found = 0;
+	for(j = 0; j < 11; j++){
+  		if(vowels[j] == str[i]){
+			printf("found vowel\n");
+			printf(noVowel);
+			found = 1;
+		}
+	}
+	if(found == 0) {
 		noVowel[nonVowelCheck] = str[i];
+		nonVowelCheck++;
 	}
-
-  }
+ }
   return noVowel;
 }
